f3e7cb29270bf34b92a44878ef16cd94b31f05d0
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2024-05-27 17:37:48 +0300

    added basic files

---
+++ lexer.py
@@ -0,0 +1,51 @@
+import re
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __repr__(self):
+        return f"Token({self.type}, {self.value})"
+
+class Lexer:
+    def __init__(self, source_code):
+        self.source_code = source_code
+        self.tokens = []
+        self.current_position = 0
+
+    def tokenize(self):
+        token_specification = [
+            ('PRINT', r'VISIBLE'),
+            ('STRING', r'"([^"\\]*(?:\\.[^"\\]*)*)"'),
+            ('NUMBER', r'\b\d+\b'),
+            ('IDENTIFIER', r'\b[a-zA-Z_]\w*\b'),
+            ('NEWLINE', r'\n'),
+            ('SKIP', r'[ \t]+'),
+            ('MISMATCH', r'.'),
+        ]
+
+        tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
+        get_token = re.compile(tok_regex).match
+        mo = get_token(self.source_code)
+        while mo is not None:
+            kind = mo.lastgroup
+            value = mo.group(kind)
+            if kind == 'NEWLINE':
+                pass
+            elif kind == 'SKIP':
+                pass
+            elif kind == 'MISMATCH':
+                raise RuntimeError(f'{value!r} unexpected')
+            else:
+                token = Token(kind, value)
+                self.tokens.append(token)
+            self.current_position = mo.end()
+            mo = get_token(self.source_code, self.current_position)
+        return self.tokens
+
+if __name__ == '__main__':
+    code = 'VISIBLE "Hello, World!"'
+    lexer = Lexer(code)
+    tokens = lexer.tokenize()
+    print(tokens)
---
+++ parser.py
@@ -0,0 +1,37 @@
+class ASTNode:
+    pass
+
+class PrintNode(ASTNode):
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return f'PrintNode({self.value})'
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.current_token_index = 0
+
+    def parse(self):
+        if self.tokens[self.current_token_index].type == 'PRINT':
+            self.current_token_index += 1
+            value = self.tokens[self.current_token_index]
+            if value.type == 'STRING':
+                self.current_token_index += 1
+                return PrintNode(value.value)
+            else:
+                raise SyntaxError("Expected a string after VISIBLE")
+        else:
+            raise SyntaxError("Unknown statement")
+
+if __name__ == '__main__':
+    from lexer import Lexer
+
+    code = 'VISIBLE "Hello, World!"'
+    lexer = Lexer(code)
+    tokens = lexer.tokenize()
+    
+    parser = Parser(tokens)
+    ast = parser.parse()
+    print(ast)

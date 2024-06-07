5372c975e7e9155642ed82fb23c8b8cf4f3d9316
xastia <122154736+xastia@users.noreply.github.com>
2024-06-07 16:23:43 +0300

    Update test_interpreter.py

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -2,11 +2,11 @@ import unittest
 from io import StringIO
 import sys
 from interpreter import Interpreter
-from parser import PrintNode
+from parser import PrintNode, SpecialCharNode
 
 class TestInterpreter(unittest.TestCase):
     def test_interpret_print(self):
-        ast = PrintNode("Hello, World!")
+        ast = [PrintNode("Hello, World!")]
         interpreter = Interpreter(ast)
         
         captured_output = StringIO()
@@ -17,6 +17,21 @@ class TestInterpreter(unittest.TestCase):
         sys.stdout = sys.__stdout__
         
         self.assertIn('Hello, World!', captured_output.getvalue())
+        
+    def test_interpret_special_chars(self):
+        ast = [SpecialCharNode('NEWLINE'), SpecialCharNode('TAB'), SpecialCharNode('BELL')]
+        interpreter = Interpreter(ast)
+        
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        
+        interpreter.interpret()
+        
+        sys.stdout = sys.__stdout__
+        
+        self.assertIn('\n', captured_output.getvalue())
+        self.assertIn('\t', captured_output.getvalue())
+        self.assertIn('\a', captured_output.getvalue())
 
 if __name__ == '__main__':
     unittest.main()

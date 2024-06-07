d1d38e0920523f44453ecd80471e1502f13d0321
xastia <122154736+xastia@users.noreply.github.com>
2024-06-07 16:23:24 +0300

    Update test_lexer.py

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -6,20 +6,31 @@ class TestLexer(unittest.TestCase):
         code = 'VISIBLE "Hello, World!" :)'
         lexer = Lexer(code)
         tokens = lexer.tokenize()
+        
         expected_tokens = [
             Token('PRINT', 'VISIBLE'),
             Token('STRING', '"Hello, World!"'),
             Token('NEWLINE', ':)')
         ]
-        for token, expected in zip(tokens, expected_tokens):
-            self.assertEqual(token.type, expected.type)
-            self.assertEqual(token.value, expected.value)
-
-    def test_tokenize_unexpected_character(self):
-        code = 'VISIBLE @'
+        
+        self.assertEqual(tokens, expected_tokens)
+    
+    def test_tokenize_special_chars(self):
+        code = 'VISIBLE "Test" :) :> :o :" ::'
         lexer = Lexer(code)
-        with self.assertRaises(RuntimeError):
-            lexer.tokenize()
+        tokens = lexer.tokenize()
+        
+        expected_tokens = [
+            Token('PRINT', 'VISIBLE'),
+            Token('STRING', '"Test"'),
+            Token('NEWLINE', ':)'),
+            Token('TAB', ':>'),
+            Token('BELL', ':o'),
+            Token('LITERAL_DOUBLE_QUOTE', ':"'),
+            Token('LITERAL_COLON', '::')
+        ]
+        
+        self.assertEqual(tokens, expected_tokens)
 
 if __name__ == '__main__':
     unittest.main()

2c19672c30afdf753b70eb3810caeb63437fe2b3
xastia <122154736+xastia@users.noreply.github.com>
2024-06-07 16:22:52 +0300

    Update test_parser.py

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,25 +1,31 @@
 import unittest
 from lexer import Lexer, Token
+from parser import Parser, PrintNode, SpecialCharNode
 
-class TestLexer(unittest.TestCase):
-    def test_tokenize_print_statement(self):
-        code = 'VISIBLE "Hello, World!" :)'
-        lexer = Lexer(code)
-        tokens = lexer.tokenize()
-        expected_tokens = [
+class TestParser(unittest.TestCase):
+    def test_parse_print_statement(self):
+        tokens = [
             Token('PRINT', 'VISIBLE'),
             Token('STRING', '"Hello, World!"'),
             Token('NEWLINE', ':)')
         ]
-        for token, expected in zip(tokens, expected_tokens):
-            self.assertEqual(token.type, expected.type)
-            self.assertEqual(token.value, expected.value)
-
-    def test_tokenize_unexpected_character(self):
-        code = 'VISIBLE @'
-        lexer = Lexer(code)
-        with self.assertRaises(RuntimeError):
-            lexer.tokenize()
-
-if __name__ == '__main__':
-    unittest.main()
+        parser = Parser(tokens)
+        ast = parser.parse()
+        
+        expected_ast = [
+            PrintNode('"Hello, World!"'),
+            SpecialCharNode('NEWLINE')
+        ]
+        
+        self.assertEqual(ast, expected_ast)
+    
+    def test_parse_special_chars(self):
+        tokens = [
+            Token('PRINT', 'VISIBLE'),
+            Token('STRING', '"Test"'),
+            Token('NEWLINE', ':)'),
+            Token('TAB', ':>'),
+            Token('BELL', ':o'),
+            Token('LITERAL_DOUBLE_QUOTE', ':"'),
+            Token('LITERAL_COLON', '::')
+        ]
